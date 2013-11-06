788d233184549404c863e128dc71326cd71e3288
mjlaforet <laforet3@uwindsor.ca>
2013-11-06 16:47:22 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,14 +6,11 @@ function serverEmail(){
 
 function dbInfo(){
 	//Username and password to login to the database
-	$user = "laforet3_311";
-	$password = "password";
-	//$user = "";
-	//$password = "";
+	$user = "";
+	$password = "";
 	
 	//Name of the database.
-	$dbname = "laforet3_311v2";
-	//$dbname = "60311";
+	$dbname = "60311";
 	//Location of the database. If on same server, likely localhost.
 	$host = "localhost";
 	
