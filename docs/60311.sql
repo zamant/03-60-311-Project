4de4eb20561cc086880acd5991f83a57365fd44e
Taphim Tousif Zaman <zamant@uwindsor.ca>
2013-11-12 09:33:52 -0500

    Update 60311.sql

--- docs/60311.sql
+++ docs/60311.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2013 at 12:41 AM
+-- Generation Time: Nov 12, 2013 at 03:31 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `authentication` (
 --
 
 INSERT INTO `authentication` (`Name`, `Token`) VALUES
-('khoda', '0a12fb50b010b3d199d4441d44a711732f3a52c0'),
-('taphim', '3a59281052497e44e72990e5728c4e8f239ae032');
+('khoda', '53eeef4cfb8872f708d7a417d56f8a0f910520a6'),
+('taphim', '16152f5c1ddf37e98a3c2644c6b1556c69c1a7d3');
 
 -- --------------------------------------------------------
 
@@ -57,24 +57,26 @@ CREATE TABLE IF NOT EXISTS `books` (
   `Price` decimal(5,2) NOT NULL DEFAULT '0.00',
   `Subject` varchar(40) COLLATE ascii_bin NOT NULL DEFAULT '',
   `Description` varchar(640) COLLATE ascii_bin NOT NULL,
-  `isbn` varchar(15) COLLATE ascii_bin DEFAULT NULL,
+  `isbn` varchar(15) COLLATE ascii_bin NOT NULL,
   `image_url` varchar(150) COLLATE ascii_bin DEFAULT NULL,
+  `contactno` bigint(10) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Secondary` (`Timestamp`,`Title`,`SellerID`),
   KEY `SellerID` (`SellerID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`ID`, `Timestamp`, `Title`, `SellerID`, `Author`, `Price`, `Subject`, `Description`, `isbn`, `image_url`) VALUES
-(1, '2013-11-03 17:47:21', 'PHP Advanced for the World Wide Web', 8, 'Larry Ullman', '0.00', '', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', NULL, 'http://d2o0t5hpnwv4c1.cloudfront.net/116_50books/16.jpg'),
-(2, '2013-11-03 08:16:21', 'Java: How to Program', 4, 'Harvey M. Deitel, Paul J. Deitel', '20.00', 'Computer Science', '', NULL, 'http://www.informit.com/ShowCover.aspx?isbn=0131486608'),
-(3, '2013-11-03 08:17:28', 'Java: How to Program', 1, 'Harvey M. Deitel, Paul J. Deitel', '15.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat.', NULL, NULL),
-(4, '2013-11-03 08:18:37', 'Java: How to Program', 2, 'Harvey M. Deitel, Paul J. Deitel', '22.00', 'Computer Science', '', NULL, NULL),
-(5, '2013-11-04 12:18:37', 'Java: How to Program', 8, 'Harvey M. Deitel, Paul J. Deitel', '22.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '1234567891234', 'http://www.computersciencelab.com/Images/cppHTP4_large.jpg'),
-(6, '2013-11-04 13:18:37', 'Digital Logic Design', 8, 'Brian Holdsworth', '45.00', 'Computer Science', 'Nulla commodo dignissim justo, et convallis magna aliquet eu. Pellentesque sollicitudin cursus turpis non viverra.', '9780750605014', 'http://www.ebook3000.com/upimg/201103/17/203332189.jpeg');
+INSERT INTO `books` (`ID`, `Timestamp`, `Title`, `SellerID`, `Author`, `Price`, `Subject`, `Description`, `isbn`, `image_url`, `contactno`) VALUES
+(1, '2013-11-03 17:47:21', 'PHP Advanced for the World Wide Web', 8, 'Larry Ullman', '0.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '', 'http://d2o0t5hpnwv4c1.cloudfront.net/116_50books/16.jpg', 5199035452),
+(2, '2013-11-03 08:16:21', 'Java: How to Program', 4, 'Harvey M. Deitel, Paul J. Deitel', '20.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '', 'http://www.informit.com/ShowCover.aspx?isbn=0131486608', 0),
+(3, '2013-11-03 08:17:28', 'Java: How to Program', 4, 'Harvey M. Deitel, Paul J. Deitel', '15.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '', NULL, 0),
+(4, '2013-11-03 08:18:37', 'Java: How to Program', 2, 'Harvey M. Deitel, Paul J. Deitel', '22.00', 'Computer Science', '', '', NULL, 0),
+(5, '2013-11-04 12:18:37', 'C++: How to Program', 8, 'Harvey M. Deitel, Paul J. Deitel', '22.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '1234567891234', 'http://www.computersciencelab.com/Images/cppHTP4_large.jpg', 5199035452),
+(6, '2013-11-04 13:18:37', 'Digital Logic Design', 8, 'Brian Holdsworth', '45.00', 'Computer Science', 'Praesent at molestie lectus. Pellentesque facilisis auctor consequat. Vestibulum eget odio quis magna placerat tempus. Mauris sed eros sollicitudin tellus bibendum lacinia facilisis id erat. Nulla commodo mi ipsum, a vehicula libero suscipit posuere. Duis sit amet massa dolor. Integer laoreet tempus aliquam. Nullam in accumsan nisl. Maecenas auctor mattis consequat. Integer ut ante vehicula, gravida dui at, iaculis purus. Donec ornare lectus vitae nunc molestie aliquet.', '9780750605014', 'http://www.ebook3000.com/upimg/201103/17/203332189.jpeg', 0),
+(7, '2013-11-04 14:18:37', 'Advanced Programming in the UNIX Environment', 9, 'W. Richard Stevens, Stephen A. Rago', '25.00', 'Computer Science', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec sollicitudin risus. Suspendisse vel commodo nulla. Maecenas vehicula vestibulum nulla, eget bibendum risus accumsan eget. Nullam tincidunt euismod nibh, at ornare ligula lobortis eu. Aliquam vitae vulputate turpis, eget volutpat metus. Nunc eu metus leo. Vestibulum ac erat nunc. Vestibulum venenatis convallis diam eu dictum. Nullam sagittis nunc sed placerat euismod. Fusce pulvinar euismod vehicula. Fusce non eros tincidunt, laoreet arcu vitae, laoreet nisi. Suspendisse consectetur quam sapien, sed ornare elit rhoncus non. Cras eget tempus odio. Sed posuere, nibh metus', '0201433079', 'http://www.ackitabevi.com/tr/Genel/rg.ashx?BELGEANAH=8130&DIL=1&RESIMISIM=KPK_0000000000.JPG', 0);
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Verified` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`ID`),
   KEY `Name` (`Name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=ascii COLLATE=ascii_bin AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `users`
@@ -104,8 +106,7 @@ INSERT INTO `users` (`ID`, `Level`, `Name`, `Password`, `Email`, `PostCode`, `Ve
 (3, 0, 'unverified', 'cb4522fe1cbc18c4e7973cfa12cbb4eb0a6158b37e824d2492d3518cafcdcf67', 'unverified@email.ca', 'N9B3P4', 0),
 (4, 0, 'parekh', 'kishan123', 'parekh@uwindsor.ca', 'N9C1A5', 1),
 (8, 0, 'taphim', '5ba865a81da464fc117519b19a6c2ada3f83656e847aa9c510bea9d936408141', 'taphim@hotmail.ca', 'N9C1G8', 1),
-(9, 0, 'khoda', '5bc26fca465aad64fda17c0c68fdf01c8b575db9fc3448d9167a2e764d4ace68', 'khoda4@hotmail.com', 'N9C3H4', 1),
-(10, 0, 'taphim3', 'db67a247809f9b986158f0d17aa5aafe6eeb67a74426946def1a5f99a3d65308', 'taphim@gmail.com', 'g4gg4g', 1);
+(9, 0, 'khoda', '5bc26fca465aad64fda17c0c68fdf01c8b575db9fc3448d9167a2e764d4ace68', 'khoda4@hotmail.com', 'N9C3H4', 1);
 
 --
 -- Constraints for dumped tables
