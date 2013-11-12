a18aa03b3497b5f7dda7c67936aa1bc8786f84db
Taphim Tousif Zaman <zamant@uwindsor.ca>
2013-11-12 10:06:24 -0500

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,10 +1,12 @@
 <?php
 require_once('includes/lib.php');
 
-$title='Search';
+$title='Search for a book';
 $article_class='search_form';
 include('includes/template/head.php');
 ?>
+<!--
+<div class="content-center">
 <form method="post" action="search.php">
 	 Title:<input type="text" name="title"/><br />
 	 Author:<input type="text" name="author"/><br />
@@ -13,6 +15,53 @@ include('includes/template/head.php');
 	 <br />
      <input type="submit" name="submit" value="Search"/>
 </form>
+</div>
+-->
+<form action="search.php" method="POST">
+			<?php 
+				if (array_key_exists('addbook',$_SESSION) && $_SESSION['addbook'] == 'error'){
+					echo '<div class="formerror">Sorry, your submission was not accepted. Please fix the errors below and resubmit.</div>';
+				}
+				echo form_text_field(
+					'Book Title:',
+					'Legal name characters including letters, numbers, or spaces.', 
+					true,
+					'title', 
+					'title',
+					'text'
+				);
+				echo "<br />";
+				echo form_text_field(
+					'Book Author:', 
+					'Legal name characters including letters, numbers, or spaces.', 
+					true,
+					'author', 
+					'author',
+					'text'
+				);
+				echo "<br />";
+				echo form_text_field(
+					'ISBN:', 
+					'ISBN', 
+					true,
+					'isbn', 
+					'isbn',
+					'text'
+				);
+				echo "<br />";
+				echo form_text_field(
+					'Subject:', 
+					'Legal name characters including letters, numbers, or spaces.', 
+					true,
+					'subject', 
+					'subject',
+					'text'
+				);
+				?><br/>
+			<div class="content-center">
+				<input type="submit" value="Search Book" />
+			</div>
+		</form>
 <?php
 
 include('includes/template/foot.php');
