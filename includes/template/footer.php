a89ddb283e8b0cd965deb647175cfe8c2dbee0a8
Taphim Tousif Zaman <zamant@uwindsor.ca>
2013-11-21 00:29:15 -0500

    Update footer.php

--- includes/template/footer.php
+++ includes/template/footer.php
@@ -3,7 +3,8 @@ Theme:
 <select id="colourselect" onchange="changeCSS(this,true)">
   <option value="css/blackandwhite.css">Black and White</option>
   <option value="css/blue.css">Blue</option>
-</select> 
+</select>
+</br></br><center>Developed by <i>TYMM</i> Copyright &#xa9; <a href="http://www1.uwindsor.ca/cs/">University of Windsor</a> 2013</center>
 <script type="text/javascript">
 	checkCookies();
 </script>
